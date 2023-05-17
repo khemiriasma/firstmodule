68b46a59dc76eaf6a2f192a7f5b60b5078ec0004
Khemiriasma <akhemiri@veoworldwide.com>
2023-05-17 14:38:00 +0000

    model commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/odata/v2/ODataModel"
     ],
-    function(BaseController) {
+    function(BaseController,ODataModel) {
       "use strict";
   
       return BaseController.extend("firstapp.firstmodule.controller.App", {
         onInit() {
+         var sUrl = "/sap/opu/odata/sap/ZWD_TEST_SRV/";
+         var oModel = new ODataModel(sUrl);
+         this.getView().setModel(oModel);
         }
       });
     }
