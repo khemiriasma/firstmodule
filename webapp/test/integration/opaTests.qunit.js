2d2ad8303a08aa8579442b8cf366d65323f90999
Asma khemiri <akhemiri@veoworldwide.com>
2023-05-17 09:50:20 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["firstapp/firstmodule/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"firstapp/firstmodule/test/unit/controller/firstView.controller"
+], function () {
+	"use strict";
+});
